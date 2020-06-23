b29b0bb0581a8696f974315ba49e325352fa112e
wfondrie <wfondrie@umaryland.edu>
2020-06-23 13:30:42 -0700

    Updated documentation and cleaned parameters

--- mokapot/brew.py
+++ mokapot/brew.py
@@ -3,61 +3,58 @@ Defines a function to run the Percolator algorithm.
 """
 import logging
 import copy
-from typing import Tuple
 from concurrent.futures import ProcessPoolExecutor
 
 import numpy as np
-import pandas as pd
 
 from .model import Model
 from .dataset import PsmDataset
-from .confidence import PsmConfidence
 
 LOGGER = logging.getLogger(__name__)
 
-# Functions -------------------------------------------------------------------
-def brew(psms: PsmDataset,
-         model: Model = Model(),
-         train_fdr: float = 0.01,
-         test_fdr: float = 0.01,
-         max_iter: int = 10,
-         folds: int = 3,
-         max_workers: int = 1) \
-         -> Tuple[PsmConfidence, Tuple[Model, ...]]:
-    """
-    Analyze a collection of PSMs using the Percolator algorithm.
 
-    The provided PSMs analyzed using the Percolator algorithm using
-    cross-validation to evaluate the held-out PSMs. If a multiple
-    collections of PSMs are provided, PSM, peptide, and protein-level
-    FDR estimates are returned for each collection.
+# Functions -------------------------------------------------------------------
+def brew(psms,
+         model=None,
+         train_fdr=0.01,
+         test_fdr=0.01,
+         max_iter=10,
+         folds=3,
+         max_workers=1):
+    """
+    Re-score one or more collection of PSMs.
+
+    The provided PSMs analyzed using the semi-supervised learning
+    algorithm that was introduced by
+    `Percolator <http://percolator.ms>`_. Cross-validation is used to
+    ensure that the learned models to not overfit to the PSMs used for
+    model training. If a multiple collections of PSMs are provided, they
+    are aggregated for model training, but the confidence estimates are
+    calculated separately for each collection.
 
     Parameters
     ----------
-    psms
-        One or more PsmDataset objects. PSMs are aggregated across
-        all of the collections for model training.
-
-    model
-        The model to be fit. The default attempts to mimic the same
-        support vector machine models used by Percolator.
-
-    train_fdr
+    psms : PsmDataset or list of PsmDataset
+        One or more :doc:`PsmDataset <dataset>` objects. PSMs are
+        aggregated across all of the collections for model training,
+        but the confidence estimates are calculated and returned
+        separately.
+    model : Model, optional
+        The :doc:`Model <model>` object to be fit. The default is
+        `None`, which attempts to mimic the same support vector machine
+        models used by Percolator.
+    train_fdr : float, optional
         The false-discovery rate threshold to define positive examples
         during model training.
-
-    test_fdr
+    test_fdr : float, optional
         The false-discovery rate threshold to evaluate whether the
         learned models yield more PSMs than the best feature.
-
-    max_iter
+    max_iter : int, optional
         The maximum number of iterations to use for training.
-
-    folds
+    folds : int, optional
         The number of cross-validation folds to use. PSMs originating
         from the same mass spectrum are always in the same fold.
-
-    max_workers
+    max_workers : int, optional
         The number of processes to use for model training. More workers
         will require more memory, but will typically decrease the total
         run time. An integer exceeding the number of folds will have
@@ -65,14 +62,16 @@ def brew(psms: PsmDataset,
 
     Returns
     -------
-    A tuple containing a PsmConfidence object and the trained
-    Classifier objects. The PsmConfidence object contains
-    false-discovery rate estimates for the PSMs, peptides,
-    and proteins.
+    PsmConfidence or list of PsmConfidence
+        An object or a list of objects containing the
+        :doc:`confidence estimates <confidence>` at various levels
+        (i.e. PSMs, peptides) when assessed using the learned score.
+        If a list, they will be in the same order as provided in the
+        `psms` parameter.
     """
+    # TODO: Add group FDR estimates.
     all_idx = set(range(len(psms.data)))
     test_idx = psms._split(folds)
-    test = sum(test_idx, [])
 
     train_sets = []
     test_sets = []
@@ -84,9 +83,6 @@ def brew(psms: PsmDataset,
         test_set = copy.copy(psms)
         test_set.data = psms.data.iloc[list(idx), :]
         test_sets.append(test_set)
-
-    print([len(x.data) for x in train_sets])
-    print([len(y.data) for y in test_sets])
 
     # Create args for map:
     map_args = [_fit_model,
@@ -102,7 +98,8 @@ def brew(psms: PsmDataset,
         else:
             map_fun = prc.map
 
-        models = [c for c in map_fun(*map_args)]
+        #models = [c for c in map_fun(*map_args)]
+        models = map_fun(*map_args)
 
     scores = [_predict(p, m, test_fdr) for p, m in zip(test_sets, models)]
     rev_idx = np.argsort(sum(test_idx, [])).tolist()
@@ -112,8 +109,19 @@ def brew(psms: PsmDataset,
 
 
 # Utility Functions -----------------------------------------------------------
-def _predict(psms: PsmDataset, model: Model, test_fdr: float):
-    """Return calibrated scores for the PSMs"""
+def _predict(psms, model, test_fdr):
+    """
+    Return calibrated scores for the PSMs.
+
+    Parameters
+    ----------
+    psms : PsmDataset
+         The PsmDataset to use.
+    model : Model
+         The model to use.
+    test_fdr : float
+         The FDR use for calibration.
+    """
     return psms._calibrate_scores(model.predict(psms), fdr_threshold=test_fdr)
 
 def _fit_model(train_set: PsmDataset, model: Model, train_fdr: float,
@@ -123,22 +131,15 @@ def _fit_model(train_set: PsmDataset, model: Model, train_fdr: float,
 
     Parameters
     ----------
-    train_set
+    train_set : PsmDataset
         A PsmDataset that specifies the training data
-
-    estimator
+    model : Model
         A Classifier to train.
-
-    train_fdr
+    train_fdr : float
         The FDR threshold used to define positive examples during the
         Percolator algorithm.
-
-    max_iter
+    max_iter : int
         The maximum number of iterations to run the algorithm.
-
-    fold
-        The fold identifier for this set. This is just used for
-        messages.
     """
     model.fit(train_set, train_fdr=train_fdr, max_iter=max_iter)
     return model
