29c317dccaf7a287ee5b00b84bb29f54b5931be3
William Fondrie <fondriew@gmail.com>
2020-11-24 01:12:05 -0800

    Fixed linting

--- mokapot/confidence.py
+++ mokapot/confidence.py
@@ -444,7 +444,12 @@ class LinearConfidence(Confidence):
             self.confidence_estimates[level] = data.loc[targets, :]
             self.decoy_confidence_estimates[level] = data.loc[~targets, :]
 
-        print((self.confidence_estimates["proteins"]["mokapot q-value"] <= 0.01).sum())
+        print(
+            (
+                self.confidence_estimates["proteins"]["mokapot q-value"]
+                <= 0.01
+            ).sum()
+        )
 
         if "proteins" not in self.confidence_estimates.keys():
             self.confidence_estimates["proteins"] = None
