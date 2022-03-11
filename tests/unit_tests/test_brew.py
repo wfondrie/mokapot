533f225a8d7c05df0328e0f62ac7d293fa24827c
William Fondrie <fondriew@gmail.com>
2022-03-11 11:01:04 -0800

    Test a single worker as a sanity check

--- tests/unit_tests/test_brew.py
+++ tests/unit_tests/test_brew.py
@@ -48,4 +48,4 @@ def test_brew_test_fdr_error(psms, svm):
 def test_brew_multiprocess(psms, svm):
     """Test that multiprocessing doesn't yield an error"""
     svm.n_jobs = 1
-    mokapot.brew(psms, svm, test_fdr=0.05, max_workers=2)
+    mokapot.brew(psms, svm, test_fdr=0.05, max_workers=1)
