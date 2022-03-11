2ae26fd9ecd3ab19a3d27cdd963dfd8c70095b24
William Fondrie <fondriew@gmail.com>
2022-03-11 11:10:14 -0800

    Test using the latest version of Python

--- tests/unit_tests/test_brew.py
+++ tests/unit_tests/test_brew.py
@@ -2,7 +2,7 @@
 import pytest
 import numpy as np
 import mokapot
-from mokapot import LinearPsmDataset, PercolatorModel
+from mokapot import PercolatorModel
 
 np.random.seed(42)
 
@@ -47,5 +47,4 @@ def test_brew_test_fdr_error(psms, svm):
 # @pytest.mark.skip(reason="Not currently working, at least on MacOS.")
 def test_brew_multiprocess(psms, svm):
     """Test that multiprocessing doesn't yield an error"""
-    svm.n_jobs = 1
-    mokapot.brew(psms, svm, test_fdr=0.05, max_workers=1)
+    mokapot.brew(psms, svm, test_fdr=0.05, max_workers=2)
