2a54e73f556b857d39ad73f722b15d1c9b81df31
J. Sebastian Paez <jspaezp@users.noreply.github.com>
2022-10-21 20:16:43 -0500

    added warning when using the ctree model

--- tests/system_tests/sample_plugin/mokapot_ctree/__init__.py
+++ tests/system_tests/sample_plugin/mokapot_ctree/__init__.py
@@ -1,10 +1,13 @@
 
 from mokapot.model import Model
 from sklearn import tree
+import logging
 
+LOGGER = logging.getLogger(__name__)
 
 class PluginModel(Model):
     def __init__(self, *args, **kwargs):
+        LOGGER.warning("The ctree model is not production ready")
         clf = tree.DecisionTreeClassifier()
         super().__init__(clf, *args, **kwargs)
 
