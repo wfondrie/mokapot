432ab83ff7f9b52d16a42181de386e707e37ead0
Will Fondrie <wfondrie@talus.bio>
2022-10-25 10:25:32 -0700

    Make Confidence objects picklable. (#78)
    * Updated for pickle
    
    * resolve merge conflicts
    
    * bump changelog
    
    * Update actions

--- tests/unit_tests/test_confidence.py
+++ tests/unit_tests/test_confidence.py
@@ -1,4 +1,6 @@
 """Test that Confidence classes are working correctly"""
+import pickle
+
 import pytest
 import numpy as np
 import pandas as pd
@@ -36,3 +38,32 @@ def test_one_group(psm_df_1000):
     scores2 = ungrouped.psms["mokapot score"]
 
     pd.testing.assert_series_equal(scores1, scores2)
+
+
+def test_pickle(psm_df_1000, tmp_path):
+    """Test that pickling works"""
+    psm_data, _ = psm_df_1000
+    psm_data["group"] = 0
+
+    psms = LinearPsmDataset(
+        psms=psm_data,
+        target_column="target",
+        spectrum_columns="spectrum",
+        peptide_column="peptide",
+        feature_columns="score",
+        filename_column="filename",
+        scan_column="spectrum",
+        calcmass_column="calcmass",
+        expmass_column="expmass",
+        rt_column="ret_time",
+        charge_column="charge",
+        copy_data=True,
+    )
+
+    results = psms.assign_confidence()
+    pkl_file = tmp_path / "results.pkl"
+    with pkl_file.open("wb+") as pkl_dat:
+        pickle.dump(results, pkl_dat)
+
+    with pkl_file.open("rb") as pkl_dat:
+        pickle.load(pkl_dat)
