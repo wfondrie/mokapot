40f83948a380fa8ad34a3358423cf9439963f0aa
sambenfredj <100685091+sambenfredj@users.noreply.github.com>
2023-05-22 14:23:23 +0200

    Fix writer tests: (#32)
    - Remove writer tests with confidence object becaause LinearPsmDataset does not have asign_confidence method anymore and results are streamed to output files while computing confidence

--- tests/unit_tests/test_writer_txt.py
+++ tests/unit_tests/test_writer_txt.py
@@ -1,30 +1,7 @@
 """Check that writing tab-delimited files works well"""
-from pathlib import Path
 
-import pytest
 import mokapot
 import pandas as pd
-
-
-def test_sanity(psms, tmp_path):
-    """Run simple sanity checks"""
-    conf = psms.assign_confidence()
-    test1 = conf.to_txt(dest_dir=tmp_path, file_root="test1")
-    mokapot.to_txt(conf, dest_dir=tmp_path, file_root="test2")
-    test3 = mokapot.to_txt([conf, conf], dest_dir=tmp_path, file_root="test3")
-    with pytest.raises(ValueError):
-        mokapot.to_txt("blah", dest_dir=tmp_path)
-
-    test4 = mokapot.to_txt(conf, dest_dir=tmp_path, decoys=True)
-    assert len(test1) == 2
-    assert len(test4) == 4
-
-    fnames = [Path(f).name for f in test1]
-    assert fnames == ["test1.mokapot.psms.txt", "test1.mokapot.peptides.txt"]
-
-    df1 = pd.read_table(test1[0])
-    df3 = pd.read_table(test3[1])
-    assert 2 * len(df1) == len(df3)
 
 
 def test_columns(mock_conf, tmp_path):
