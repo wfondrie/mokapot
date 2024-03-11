896611570da2242e8c4f1d41b384c6ff6d0f783b
Elmar Zander <elmar@zandere.de>
2024-03-11 18:59:57 +0100

    Make cli unit tests for aggregateP2P easier debuggable

--- tests/system_tests/test_aggregatePsmsToPeptide.py
+++ tests/system_tests/test_aggregatePsmsToPeptide.py
@@ -4,19 +4,39 @@ These tests verify that the aggregatePsmsToPeptides executable works as expected
 
 import subprocess
 from pathlib import Path
+from mokapot.aggregatePsmsToPeptides import main as aggregate_main
 
 import pytest
 
 # Warnings are errors for these tests
 pytestmark = pytest.mark.filterwarnings("error")
 
+def run_aggreate_cli(params, run_in_subprocess=True):
+    """
+    Run the aggregatePsmsToPeptides command either in a subprocess or as direct
+    call to the main function.
+
+    Parameters
+    ----------
+    params : list
+        List of parameters to be passed to the `mokapot.aggregatePsmsToPeptides` command line tool.
+
+    run_in_subprocess : bool, optional
+        Indicates whether the command should be run in a separate subprocess. Default is `False`.
+    """
+    if run_in_subprocess:
+        cmd = [
+            "python",
+            "-m",
+            "mokapot.aggregatePsmsToPeptides"] + params
+        subprocess.run(cmd, check=True)
+    else:
+        aggregate_main([str(c) for c in params])
+
 
 def test_basic_cli(tmp_path, targets_decoys_psms_scored):
     """Test that basic cli works."""
     cmd = [
-        "python",
-        "-m",
-        "mokapot.aggregatePsmsToPeptides",
         "--targets_psms",
         targets_decoys_psms_scored[0],
         "--decoys_psms",
@@ -24,16 +44,13 @@ def test_basic_cli(tmp_path, targets_decoys_psms_scored):
         "--dest_dir",
         tmp_path,
     ]
-    subprocess.run(cmd, check=True)
+    run_aggreate_cli(cmd)
     assert Path(tmp_path, "targets.peptides").exists()
 
 
 def test_cli_keep_decoys(tmp_path, targets_decoys_psms_scored):
     """Test that --keep_decoys works."""
     cmd = [
-        "python",
-        "-m",
-        "mokapot.aggregatePsmsToPeptides",
         "--targets_psms",
         targets_decoys_psms_scored[0],
         "--decoys_psms",
@@ -42,7 +59,7 @@ def test_cli_keep_decoys(tmp_path, targets_decoys_psms_scored):
         tmp_path,
         "--keep_decoys",
     ]
-    subprocess.run(cmd, check=True)
+    run_aggreate_cli(cmd)
     assert Path(tmp_path, "targets.peptides").exists()
     assert Path(tmp_path, "decoys.peptides").exists()
 
@@ -50,9 +67,6 @@ def test_cli_keep_decoys(tmp_path, targets_decoys_psms_scored):
 def test_non_default_fdr(tmp_path, targets_decoys_psms_scored):
     """Test non-defaults"""
     cmd = [
-        "python",
-        "-m",
-        "mokapot.aggregatePsmsToPeptides",
         "--targets_psms",
         targets_decoys_psms_scored[0],
         "--decoys_psms",
@@ -64,6 +78,6 @@ def test_non_default_fdr(tmp_path, targets_decoys_psms_scored):
         "--keep_decoys",
     ]
 
-    subprocess.run(cmd, check=True)
+    run_aggreate_cli(cmd)
     assert Path(tmp_path, "targets.peptides").exists()
     assert Path(tmp_path, "decoys.peptides").exists()
