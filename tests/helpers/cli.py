4b9784f8af1151fdd7fbf4463443cc8696c937aa
Elmar Zander <elmar@zandere.de>
2024-05-08 14:45:46 +0200

    Correct problem in unit test with log output and typechecking

--- tests/helpers/cli.py
+++ tests/helpers/cli.py
@@ -1,11 +1,44 @@
+import contextlib
+import logging
 import os
 import io
 import subprocess
+import sys
 from contextlib import redirect_stdout, redirect_stderr
 from typing import List, Any, Optional, Dict, Callable
 
+import pytest
+from _pytest.logging import caplog, LogCaptureFixture
+from typeguard import TypeCheckError
+
 from mokapot.mokapot import main as mokapot_main
 from mokapot.aggregatePsmsToPeptides import main as aggregate_main
+
+@contextlib.contextmanager
+def catch_type_check():
+    """
+    Catch Type Check
+
+    Context manager that catches TypeCheckError exceptions and prints detailed error information.
+
+    Usage:
+    ------
+    with catch_type_check():
+        # code that may raise TypeCheckError
+    """
+    try:
+        yield None
+    except TypeCheckError as e:
+        import traceback, sys
+        print("\n\nTypeCheckError: ", end="")
+        # Print the exception with the stack trace excluding the last
+        # entries which stem from the typeguard module
+        frames = traceback.extract_tb(e.__traceback__)
+        entries_to_print = sum(1 for frame in frames
+                               if not "typeguard" in frame.filename)
+        traceback.print_exception(e, limit=entries_to_print)
+        raise
+
 
 
 def _run_cli(module: str, main_func: Callable, params: List[Any],
@@ -36,25 +69,37 @@ def _run_cli(module: str, main_func: Callable, params: List[Any],
         value = os.environ.get('TEST_CLI_IN_SUBPROCESS', 'true')
         run_in_subprocess = value.lower() in ['true', 'yes', 'y', '1']
     else:
-        run_in_subprocess = True
+        run_in_subprocess = bool(run_in_subprocess)
 
     params = [str(param) for param in params]
     if run_in_subprocess:
         cmd = ["python", "-m", module] + params
-        res = subprocess.run(cmd, check=True, capture_output=capture_output)
+        try:
+            res = subprocess.run(cmd, check=True, capture_output=capture_output)
+        except subprocess.CalledProcessError as e:
+            logging.error(f"Calling {module} with params {params} failed\nStderr: {e.stderr}")
+            raise RuntimeError(f"Calling {module} with params {params} failed") from e
         if capture_output:
             return {'stdout': res.stdout.decode(),
                     'stderr': res.stderr.decode()}
     elif capture_output:
         stdout_sink = io.StringIO()
         stderr_sink = io.StringIO()
-        with redirect_stdout(stdout_sink), redirect_stderr(stderr_sink):
-            main_func(params)
+        logger = logging.getLogger()
 
+        handler = logging.StreamHandler(stderr_sink)
+        handler.setFormatter(logging.Formatter("[%(levelname)s] %(message)s"))
+        logger.addHandler(handler)
+        logger.setLevel(logging.DEBUG)
+
+        with redirect_stderr(stderr_sink), redirect_stdout(stdout_sink):
+            with catch_type_check():
+                main_func(params)
         return {'stdout': stdout_sink.getvalue(),
                 'stderr': stderr_sink.getvalue()}
     else:
-        main_func(params)
+        with catch_type_check():
+            main_func(params)
 
 
 def run_mokapot_cli(params: List[Any], run_in_subprocess=None,
