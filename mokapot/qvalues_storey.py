46b493da32e17f8d16950cac061a0d9661f2bab4
Elmar Zander <elmar.zander@googlemail.com>
2024-12-13 20:38:05 +0100

    Finish Storey's qvalue algorithm

--- mokapot/qvalues_storey.py
+++ mokapot/qvalues_storey.py
@@ -3,8 +3,12 @@ from logging import warning
 
 import numpy as np
 import scipy as sp
+from typeguard import typechecked
+
+from mokapot.peps import monotonize_simple
 
 
+@typechecked
 def empirical_pvalues(
     s: np.ndarray[float], s0: np.ndarray[float], *, mode: str = "best"
 ) -> np.ndarray[float]:
@@ -35,6 +39,13 @@ def empirical_pvalues(
     -------
     np.ndarray[float]
         Array of empirical p-values corresponding to the input data array `s`.
+
+    References
+    ----------
+
+    .. [1] B V North, D Curtis, P C Sham, A Note on the Calculation of
+       Empirical P Values from Monte Carlo Procedures,  Am J Hum Genet, 2003
+       Feb;72(2):498–499. doi: 10.1086/346173
     """
     N = len(s0)
     emp_null = sp.stats.ecdf(s0)
@@ -52,15 +63,56 @@ def empirical_pvalues(
         )
 
 
+@typechecked
+def count_larger(pvals, lambdas):
+    """
+    Counts the number of elements in `pvals` that are greater than or equal to
+    each value in `lambdas`.
+
+    This function calculates the cumulative count of `pvals` values that are
+    larger than or equal to each quantile in the `lambdas` array. The `lambdas`
+    array is expected to be in strictly ascending order.
+
+    Parameters
+    ----------
+    pvals : array_like
+        Array of values for which cumulative counts are calculated against the
+        `lambdas` thresholds.
+
+    lambdas : array_like
+        Array of threshold values in strictly ascending order. The counts of
+        `pvals` larger than or equal to these values will be computed.
+
+    Returns
+    -------
+    cumulative_counts : ndarray
+        Array of cumulative counts, where each element corresponds to the count
+        of `pvals` that are greater than or equal to the corresponding value in `lambdas`.
+    """
+    assert np.all(lambdas[1:] > lambdas[:-1])
+    bin_edges = np.append(lambdas, np.inf)
+    hist_counts, _ = np.histogram(pvals, bins=bin_edges)
+    cumulative_counts = np.cumsum(hist_counts[::-1])[::-1]
+    return cumulative_counts
+
+
+Pi0Est = namedtuple("Pi0Est", ["pi0", "pi0s_smooth", "pi0s_raw", "lambdas", "mse"])
+
+
+@typechecked
 def estimate_pi0(
-    p: np.ndarray[float], *, method: str = "smoother", lambdas=np.arange(0.05, 1, 0.05)
-) -> np.ndarray[float]:
+    pvals: np.ndarray[float],
+    *,
+    method: str = "smoother",
+    lambdas: np.ndarray[float] = np.arange(0.2, 0.8, 0.01),
+    eval_lambda: float = 0.5,
+) -> Pi0Est:
     """
     Estimate pi0 from p-value using Storey's method.
 
     Parameters
     ----------
-    p : np.ndarray[float]
+    pvals : np.ndarray[float]
         Array of p-values for which the proportion of null hypotheses (pi0) is
         estimated.
     method : str, optional
@@ -81,38 +133,118 @@ def estimate_pi0(
         Array of raw pi0 estimates.
     - lambdas : np.ndarray[float]
         Array of lambdas used to estimate pi0.
+
+    References
+    ----------
+    .. [1] John D. Storey, Robert Tibshirani, Statistical significance for
+        genomewide studies,pp. 9440 –9445 PNAS August 5, 2003, vol. 100, no. 16
+        www.pnas.org/cgi/doi/10.1073/pnas.1530509100
+    .. [2] Storey JD, Bass AJ, Dabney A, Robinson D (2024). qvalue: Q-value
+        estimation for false discovery rate control. R package version 2.38.0,
+        http://github.com/jdstorey/qvalue.
     """
-    N = len(p)
+    N = len(pvals)
     lambdas = np.sort(lambdas)
     L = len(lambdas)
 
-    assert min(p) >= 0 and max(p) <= 1
+    assert min(pvals) >= 0 and max(pvals) <= 1
     assert min(lambdas) >= 0 and max(lambdas) <= 1
-    assert len(lambdas) == 1 or len(lambdas) >= 4
+    assert len(lambdas) >= 4
 
-    if max(p) < max(lambdas):
+    if max(pvals) < max(lambdas):
         warning(
-            f"The maximum p-value ({max(p)}) should be larger than the "
+            f"The maximum p-value ({max(pvals)}) should be larger than the "
             f"maximum lambda ({max(lambdas)})"
         )
 
-    # Find for each p in which interval i given by lambdas[i-1]<=p<lambdas[i]
-    # is located (set lambdas[-1]==-infinity)
-    interval_indices = np.searchsorted(lambdas, p, side="right")
+    pi0s_smooth = pi0s_raw = mse = None
 
-    # Count for each interval, how many p-values fall into it (drop the
-    # [-infty,0.05] interval)
-    interval_counts = np.bincount(interval_indices, minlength=L + 1)[1 : L + 1]
+    if method == "fixed":
+        pvals_exceeding_lambda = sum(pvals >= eval_lambda)
+        pi0 = pvals_exceeding_lambda / (N * (1.0 - eval_lambda))
+    else:
+        # Estimate raw pi0 values ("contaminated" for small lambdas by the true
+        # target distribution
+        W = count_larger(pvals, lambdas)
+        pi0s_raw = W / (N * (1.0 - lambdas))
 
-    # Estimate raw pi0 values ("contaminated" for small lambdas by the true
-    # target distribution
-    pvals_exceeding_lambda = np.flip(np.cumsum(np.flip(interval_counts)))
-    pi0s = pvals_exceeding_lambda / (N * (1.0 - lambdas))
+        if method == "smoother":
+            # Now smooth it with a smoothing spline and evaluate
+            pi0_spline_est = sp.interpolate.UnivariateSpline(
+                lambdas, pi0s_raw, k=3, ext=0
+            )
+            pi0s_smooth = pi0_spline_est(lambdas)
+            pi0 = pi0_spline_est(eval_lambda)
+        elif method == "bootstrap":
+            pi0_min = np.quantile(pi0s_raw, 0.1)
+            mse = (
+                W / (N**2 * (1 - lambdas) ** 2) * (1 - W / N)
+                + (pi0s_raw - pi0_min) ** 2
+            )
+            pi0 = pi0s_raw[np.argmin(mse)]
+        else:
+            raise ValueError(f"Unknown pi0-estimation method {method}")
 
-    # Now smooth it with a smoothing spline and evaluate
-    smoothed_pi0 = sp.interpolate.UnivariateSpline(lambdas, pi0s, k=3, ext=0)
-    pi0s_smooth = smoothed_pi0(lambdas)
-    pi0 = pi0s_smooth[-1]
+    pi0 = np.clip(pi0, 0, 1)
 
-    Pi0Est = namedtuple("Pi0Est", ["pi0", "pi0s_smooth", "pi0s_lambda", "lambdas"])
-    return Pi0Est(pi0, pi0s_smooth, pi0s, lambdas)
+    return Pi0Est(pi0, pi0s_smooth, pi0s_raw, lambdas, mse)
+
+
+@typechecked
+def qvalues(
+    pvals: np.ndarray[float],
+    *,
+    pi0: float | None = None,
+    small_p_correction: bool = False,
+) -> np.ndarray[float]:
+    """
+    Calculates q-values, which are an estimate of false discovery rates (FDR),
+    for an array of p-values. This function is based on Storey's implementation
+    in the R package "qvalue".
+
+    Parameters
+    ----------
+    pvals : np.ndarray[float]
+        An array of p-values to compute q-values for. The array should contain
+        values between 0 and 1.
+    pi0 : float, optional
+        Proportion of true null hypotheses. If not provided, it is estimated
+        using the bootstrap method (see estimate_pi0).
+    small_p_correction : bool, optional
+        Whether to apply a small p-value correction (Storey's pfdr parameter),
+        which adjusts for very small p-values in the dataset.
+
+    Returns
+    -------
+    np.ndarray[float]
+        An array of q-values corresponding to the input p-values. The q-values
+        are within the range [0, 1].
+
+    References
+    ----------
+    .. [1] John D. Storey, Robert Tibshirani, Statistical significance for
+        genomewide studies,pp. 9440 –9445 PNAS August 5, 2003, vol. 100, no. 16
+        www.pnas.org/cgi/doi/10.1073/pnas.1530509100
+    .. [2] John D. Storey, A direct approach to false discovery rates,
+        J. R. Statist. Soc. B (2002), 64, Part 3, pp. 479–498
+    .. [3] Storey JD, Bass AJ, Dabney A, Robinson D (2024). qvalue: Q-value
+        estimation for false discovery rate control. R package version 2.38.0,
+        http://github.com/jdstorey/qvalue.
+    """
+    N = len(pvals)
+    order = np.argsort(pvals)
+    pvals_sorted = pvals[order]
+    if pi0 is None:
+        pi0 = estimate_pi0(pvals, method="bootstrap")
+
+    fdr_sorted = pi0 * pvals_sorted * N / np.linspace(1, N, N)
+    if small_p_correction:
+        fdr_sorted /= 1 - (1 - pvals) ** N
+
+    qvalues_sorted = monotonize_simple(fdr_sorted, ascending=True, reverse=True)
+
+    qvalues = np.zeros_like(qvalues_sorted)
+    qvalues[order] = qvalues_sorted
+
+    qvalues = np.clip(qvalues, 0.0, 1.0)
+    return qvalues
--- tests/unit_tests/test_qvalues_storey.py
+++ tests/unit_tests/test_qvalues_storey.py
@@ -1,6 +1,10 @@
-import numpy as np
+import json
 
-from mokapot.qvalues_storey import empirical_pvalues
+import numpy as np
+from pytest import approx
+
+from mokapot.qvalues_storey import empirical_pvalues, estimate_pi0
+from qvalues_storey import qvalues
 
 
 def test_empirical_pvalues():
@@ -9,8 +13,10 @@ def test_empirical_pvalues():
     s = np.arange(0, 13)
     s0 = np.arange(-99, 101)
     p = 0.5 - s / len(s0)
+
     def empPvals(s, s0):
-        return empirical_pvalues(s, s0, mode = "storey")
+        return empirical_pvalues(s, s0, mode="storey")
+
     np.testing.assert_almost_equal(empPvals(s, s0), p)
 
     # Now shift and compare
@@ -32,3 +38,46 @@ def test_empirical_pvalues():
     p = np.clip((101 - s) / (1 + N), 1.0 / (N + 1), 1)
     np.testing.assert_almost_equal(empirical_pvalues(s, s0, mode="best"), p)
 
+
+def test_estimate_pi0_from_R():
+    # with open("data/samples.json", "r") as file:
+    with open("data/hedenfalk.json", "r") as file:
+        data = json.load(file)
+    targets = np.array(data["stat"])
+    decoys = np.array(data["stat0"])
+
+    # Preliminary check that pvalues match
+    pvalues = empirical_pvalues(targets, decoys, mode="storey")
+    pvals_exp = np.array(data["pvalues"])
+    np.testing.assert_almost_equal(pvalues, pvals_exp)
+
+    # Compute pi0 with bootstrap method
+    lambdas = np.arange(0.05, 1, 0.05)
+    pi0est = estimate_pi0(pvalues, method="bootstrap", lambdas=lambdas)
+    assert pi0est.pi0 == approx(0.6763407)
+    assert pi0est.mse is not None
+    assert pi0est.pi0s_raw[np.argmin(pi0est.mse)] == pi0est.pi0
+
+    # Compute pi0 by smoothing (the comparison value is from the qvalue package
+    # in R, since the smoothing method is different the difference is relatively
+    # large)
+    lambdas = np.arange(0.2, 0.95, 0.01)
+    pi0est = estimate_pi0(pvalues, lambdas=lambdas, eval_lambda=0.8)
+    assert pi0est.pi0 == approx(0.6931328, abs=0.01)
+
+    # Compute pi0 with fixed lambda
+    pi0est = estimate_pi0(pvalues, method="fixed", eval_lambda=0.7)
+    assert pi0est.pi0 == approx(0.701367)
+    pi0est = estimate_pi0(pvalues, method="fixed", eval_lambda=0.3)
+    assert pi0est.pi0 == approx(0.7138351)
+
+
+def test_qvalues_storey():
+    with open("data/hedenfalk.json", "r") as file:
+        data = json.load(file)
+    pvals = np.array(data["pvalues"])
+    qvals_expect = np.array(data["qvalues"])
+
+    pi0est = estimate_pi0(pvals, method="bootstrap", lambdas=np.arange(0.05, 1, 0.05))
+    qvals = qvalues(pvals, pi0=pi0est.pi0)
+    np.testing.assert_almost_equal(qvals, qvals_expect)
